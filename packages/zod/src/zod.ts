79dd44499bf325b297b0e12aa73b9f88f2dba1b4
omsharma29 <theomsharma100@gmail.com>
2025-03-03 09:18:33 +0530

    vercel deploy try 9

--- packages/zod/src/zod.ts
+++
@@ -1,8 +0,0 @@
-import {z} from "zod"
-
-export const UserSchema = z.object({
-    email: z.string().email("Invalid email format"),
-    password : z.string().min(6, "Password Must be min 6 character")
-})
-
-export type LoginSchema = z.infer<typeof UserSchema>
